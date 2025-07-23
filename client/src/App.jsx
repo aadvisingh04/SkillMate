b5a891e29ba253295ecd02926dade929f90d2b84
aadvisingh04 <aadvisingh01@gmail.com>
2025-07-24 00:45:52 +0530

    Integrated SkillMate frontend, corrected paths, and confirmed main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Features from './pages/Features'
 import './App.css'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
+import Dashboard from './pages/Dashboard'
+import Roadmap from './pages/Roadmap'
+import CourseLibrary from './pages/CourseLibrary'
+import { Routes, Route } from 'react-router-dom'
+import AIChat from './pages/AIChat'
+import Quiz from './pages/Quiz'
+import QuizzesPage from './pages/QuizzesPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Features />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/roadmap/:name" element={<Roadmap />} />
+        <Route path="/library" element={<CourseLibrary />} />
+        <Route path="/ai-chat" element={<AIChat />} />
+        <Route path="/quiz/:topic" element={<Quiz />} />
+        <Route path="/quizzes" element={<QuizzesPage />} />
+      </Routes>
+    </div>
   )
 }
 
