b0eb728a088b19bd63f2233f4601c9b47c68fc44
aadvisingh04 <aadvisingh01@gmail.com>
2025-07-29 09:26:49 +0530

    Copied actual SkillMate frontend source code and re-deployed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css' // This line correctly imports your Tailwind CSS and custom animations from index.css
-
+import './index.css'
+import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
-import App from './App.jsx' // This correctly imports your App component from App.jsx
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App /> {/* This renders your SkillMate App component */}
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
