566d547be2e23218701b1ade4973728b4b4e28c3
aadvisingh04 <aadvisingh01@gmail.com>
2025-07-29 09:04:56 +0530

    Frontend build successful locally, pushing for Render deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css' // This line correctly imports your Tailwind CSS and custom animations from index.css
 
 import { BrowserRouter } from 'react-router-dom'
+
+import App from './App.jsx' // This correctly imports your App component from App.jsx
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <App /> {/* This renders your SkillMate App component */}
     </BrowserRouter>
   </StrictMode>,
 )
